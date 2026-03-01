33327490f8058b54d412a0af79fea16c4d1df4a4
wukongdaily <2666180@gmail.com>
2026-03-01 16:36:51 +0800

    pw 默认自带xray 和  hysteria

--- raspberrypi/24.10/build.sh
+++ raspberrypi/24.10/build.sh
@@ -75,7 +75,7 @@ PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 #24.10
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES xray-core hysteria luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
