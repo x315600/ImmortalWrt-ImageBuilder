29a1267f7dd57fb093db14fa8a2575da81952f2a
wukongdaily <2666180@gmail.com>
2026-03-01 15:22:49 +0800

    新增watchdog
    https://github.com/sirpdboy/luci-app-watchdog

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -32,6 +32,8 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-tailscale luci-i18n-tailscale-zh-cn"
 # 分区扩容 by sirpdboy 
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp luci-i18n-partexp-zh-cn"
+# 看门狗watchdog by sirpdboy
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-watchdog luci-i18n-watchdog-zh-cn"
 # 酷猫主题 by sirpdboy 
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-kucat"
 # 进阶设置 by sirpdboy 
