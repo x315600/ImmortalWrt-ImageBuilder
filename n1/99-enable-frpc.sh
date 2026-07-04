5cfcda02aa71705b9b2288b21938c0adc73aeae0
x315600 <315601qq@gmail.com>
2026-07-04 21:07:17 +0800

    Update 99-enable-frpc.sh

--- n1/99-enable-frpc.sh
+++ n1/99-enable-frpc.sh
@@ -5,7 +5,7 @@ echo "Enabling frpc service at $(date)" >> "$LOGFILE"
 
 # Enable frpc daemon
 uci set frpc.config.enabled='1'
-uci set frpc.config.server_addr='38.14.254.183'
+uci set frpc.config.server_addr='38.14.'
 uci set frpc.config.server_port='7000'
 uci set frpc.config.token='315600'
 uci commit frpc
